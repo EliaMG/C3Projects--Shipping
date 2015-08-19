49edbf880d34d43763fd5e8a81f7432329fa408f
Elia <elgrenier@gmail.com>
2015-08-19 11:50:00 -0700

    writing tests to make sure page renders

--- spec/controllers/shippings_controller_spec.rb
+++ spec/controllers/shippings_controller_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe ShippingsController, type: :controller do
+  describe "get #quotes" do
+    before :each do
+      get :quotes, order:
+      "origin=Texarkana TX 75505 US&destination=Seattle WA 98101 US&packages={quantity: 4, weight: 5, length: 6, height: 6, width: 6}, {quantity: 3, weight: 5, length: 6, height: 6, width: 6}, {quantity: 2, weight: 5, length: 6, height: 6, width: 6}"
+    end
 
+    it "is successful" do
+      expect(response.response_code).to eq 200
+    end
+
+    it "returns json" do
+      expect(response.header["Content-Type"]).to include 'application/json'
+    end
+  end
 end
