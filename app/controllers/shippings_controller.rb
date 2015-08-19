f7bec78b236995b9b0728fc61cfb87c2ca523ed1
Loraine <loraine@lorainekv.com>
2015-08-19 16:42:44 -0700

    Created a munge_request method.

--- app/controllers/shippings_controller.rb
+++ app/controllers/shippings_controller.rb
@@ -1,6 +1,35 @@
 class ShippingsController < ApplicationController
 
   def quotes
-    render json: {party: "everywhere"}
+
+    @request = request.parameters
+    render json: {your_params: request.parameters, party: @munged}
   end
+
+  private
+
+  def munge_request(request)
+
+  city = request[:origin][:origin_city]
+  state = request[:origin][:origin_state]
+  zip = request[:origin][:origin_zip]
+  country = request[:origin][:origin_country]
+
+  @origin = ActiveShipping::Location.new(country: country,
+                                         state: state,
+                                         city: city,
+                                         zip: zip
+                                        )
+  
+  end
+
+
+  def get_fedex
+
+  end
+
+  def get_ups
+
+  end
+
 end
