6946453d87247f0ea96db2291b868d6b6e6e4752
Elia <elgrenier@gmail.com>
2015-08-19 18:43:01 -0700

    changes to spec test

--- spec/controllers/shippings_controller_spec.rb
+++ spec/controllers/shippings_controller_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe ShippingsController, type: :controller do
 
   describe "get #quotes" do
     before :each do
-      get :quotes, origin: {:origin_city =>"Texarkana", :origin_state =>"TX", :origin_zip =>"75505", :origin_country =>"US"}, destination: {:destination_city =>"Seattle", :destination_state =>"WA", :destination_zip =>"98115", :destination_country =>"US"}, packages: {1 =>{:quantity => "1", :weight => "6794.0", :length => "4.0", :width =>"13.0", :height =>"70.0"}}
+      get :quotes, origin: {:origin_city =>"Texarkana", :origin_state =>"TX", :origin_zip =>"75505", :origin_country =>"US"}, destination: {:destination_city =>"Seattle", :destination_state =>"WA", :destination_zip =>"98115", :destination_country =>"US"}, packages: {1 =>{:quantity => "1", :weight => "6794.0", :length => "4.0", :width =>"13.0", :height =>"70.0"}, 2 =>{:quantity => "2", :weight => "6794.0", :length => "4.0", :width =>"13.0", :height =>"70.0"}}
     end
 
     it "is successful" do
