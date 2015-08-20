c22a6ef20cfa04be8f06321d009ca58f0904b452
Elia <elgrenier@gmail.com>
2015-08-19 17:49:15 -0700

    methods for getting ups quote

--- app/controllers/shippings_controller.rb
+++ app/controllers/shippings_controller.rb
@@ -1,8 +1,9 @@
 class ShippingsController < ApplicationController
 
   def quotes
-
     @request = request.parameters
+    ups_response = get_ups_quote(@request)
+    fedex_response = get_fedex_quote(@request)
     render json: {your_params: request.parameters, party: @munged}
   end
 
@@ -38,8 +39,7 @@ class ShippingsController < ApplicationController
 
   def munge_packages(request)
 
-    @packages = []
-
+    package_array = []
     index = 1
 
     request[:packages].each do |package|
@@ -52,27 +52,30 @@ class ShippingsController < ApplicationController
       new_package = ActiveShipping::Package.new(weight, [height, length, width])
 
       index +=1
-      @packages << new_package
+      package_array << new_package
     end
 
-    @packages
-
-      # @packages
-      # package = request[:packages][1]
-      #
-      # weight = package[:weight].to_i
-      # height = package[:height].to_i
-      # length = package[:length].to_i
-      # width = package[:width].to_i
+    @packages = package_array
   end
 
 
-  def get_fedex
+  def get_fedex_quote
 
   end
 
-  def get_ups
+  def get_ups_quote(request)
+    munge_origin(request)
+    munge_destination(request)
+    munge_packages(request)
 
+    ups = ups_cred
+    ups_quote = ups.find_rates(@origin, @destination, @packages)
+
+  end
+
+  def ups_cred
+    ActiveShipping::UPS.new(login: ENV["UPS_LOGIN"], password: ENV["UPS_PASSWORD"],
+    key: ENV["UPS_KEY"])
   end
 
 end
